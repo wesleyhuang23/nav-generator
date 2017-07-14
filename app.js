98b4b5447c36f5c440f0042a5dfb885e4a2c1b63
Wesley <wesley@gooddogdesign.com>
2017-07-14 10:57:00 -0700

    able to add mobile nav items

--- app.js
+++ app.js
@@ -27,6 +27,22 @@ $(function(){
             var name = form.children[1].value;
             var id = form.children[3].value;
             $('.desktop-item-output').text(`<li>{{widget type="catalog/category_widget_link" anchor_text="`+ name +`" title="`+ name +`" template="catalog/category/widget/link/link_block.phtml" id_path="category/`+ id +`"}}</li>`);
+        } else if(form.className === 'mobile-title'){
+            var name = form.children[1].value;
+            $('.mobile-title-output').text(`
+                <li class="nav-mobile--dig">`+ name +`</li>
+                    <ul class="nav--mobile nav-mobile--level-two">
+                        <li class="nav-mobile--back">Back</li>
+                        <!--PUT MOBILE NAV ITEMS HERE-->
+
+                    </ul>
+            `)
+        } else if(form.className === 'mobile-item'){
+            var name = form.children[1].value;
+            var link = form.children[3].value;
+            $('.mobile-item-output').text(`
+                <li><span class="widget widget-category-link"><a href="http://www.guideboat.com/`+ link +`" title="`+ name +`"><span>` + name + `</span></a></span></li>
+            `)
         }
     })
 })
