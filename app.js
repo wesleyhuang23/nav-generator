68587f3a8c23fbfdbed58e211aefffbc65f15dfb
Wesley <wesley@gooddogdesign.com>
2017-07-14 09:06:43 -0700

    able to generate html for title

--- app.js
+++ app.js
@@ -2,4 +2,25 @@ $(function(){
     $('.editable').each(function(){
         this.contentEditable = true;
     })
+
+    $('button').on('click', function(e){
+        console.log(e.target.parentElement.children[1].value);
+        var title = e.target.parentElement.children[1].value;
+        $('.desktop-title').text(`
+            <li>
+                <a style="text-decoration: none; cursor: default;" title="`+ title + `"> `+ title +`</a>
+                <div class="dropdown">
+                    <ul>
+                        <li>
+                            <ul class="dropdown2">
+
+                            <!--PUT NAV ITMES HERE -->
+
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        `)
+    })
 })
